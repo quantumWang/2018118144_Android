26cef2197c438587c900487b29ecdc8776aae658
LAPTOP-TL2J01OC\10108 <quantumwamg@gmail.com>
2020-12-24 22:38:36 +0800

    update basic of usage project
    上传了服务与活动之间的通信的实例代码。

---
+++ Basic usage of services/MyServiceTest/app/src/main/java/com/example/myservicetest/MyService.java
@@ -0,0 +1,54 @@
+package com.example.myservicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    //    MyService继承Service类，说明这是一个服务
+
+    private DownLoadBinder mBinder = new DownLoadBinder();
+
+    class DownLoadBinder extends Binder {
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+
+    }
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+//        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
+    }
+}
